4cd3dd82e52bb17612d17a8e278afafd65bcbbc3
SANGMIN <kwk680037@gmail.com>
2023-11-05 22:20:38 +0900

    2023.11.05(일) PM 10:19 모던자바스크립트 DeepDive 예재 (Vscode Git 연동)

--- 모던자바스크립트 DeepDive/09/09-21.js
+++ 모던자바스크립트 DeepDive/09/09-21.js
@@ -5,3 +5,9 @@ var message = '';
 if (!done) message = '미완료';
 
 console.log(message);
+
+// if 문은 단축평가로 대체 가능하다.
+// done 이 false 라면 message에 '미완료'를 할당
+
+message = done || '미완료';
+console.log(message);
